1e2e95f9911b418ed431fa5cda3b0f08fe023df7
2013doofazit58 <60706527+2013doofazit58@users.noreply.github.com>
2023-07-24 00:17:25 +0600

    first

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,67 @@
+<script setup>
+import{ref,reactive} from 'vue'
+const findex=ref(0)
+const currentIndex=ref()
+
+const fullImage = ref('https://images.unsplash.com/photo-1682685797769-481b48222adf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60')
+const images = [
+  {
+    thumb: "https://images.unsplash.com/photo-1682685797769-481b48222adf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=300&q=60",
+    full: "https://images.unsplash.com/photo-1682685797769-481b48222adf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60"
+  },
+  {
+    thumb: "https://images.unsplash.com/photo-1682695794816-7b9da18ed470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=300&q=60",
+    full: "https://images.unsplash.com/photo-1682695794816-7b9da18ed470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60"
+  },
+  {
+    thumb: "https://images.unsplash.com/photo-1682685797661-9e0c87f59c60?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=300&q=60",
+    full: "https://images.unsplash.com/photo-1682685797661-9e0c87f59c60?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60"
+  },
+  {
+    thumb: "https://plus.unsplash.com/premium_photo-1666963323736-5ee1c16ef19d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=300&q=60",
+    full: "https://plus.unsplash.com/premium_photo-1666963323736-5ee1c16ef19d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60"
+  }
+]
+
+let length=images.length-1
+console.log(length)
+
+const changeFullImage=(image)=>{
+  fullImage.value=image.full
+}
+function next(){
+ findex.value = findex.value+1
+ if(findex.value>length){
+  findex.value=0
+ }
+ 
+ fullImage.value=images[findex.value].full
+}
+function previous(){
+ findex.value = findex.value-1
+ if(findex.value<0){
+  findex.value=length-1
+ }
+ fullImage.value=images[findex.value].full
+}
+
+</script>
+
+<template>
+  
+  <section class="container mx-auto flex items-center flex-col">
+        <h1 class="text-center text-2xl py-10">Image carousel</h1>
+        <div class="">
+              <div >
+             
+                <img class="cursor-pointer w-full h-48" :src="fullImage" alt="">
+                <button @click="previous()">previous</button>
+                <button @click="next()">next</button>
+                
+            </div>
+            
+        </div>
+    </section>
+</template>
+
+<style scoped></style>
